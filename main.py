468f447aa4d3cb2df5bded7f2973e2897e28b522
rajeshmauryasde <rajeshmauryasde@gmail.com>
2023-02-06 12:33:38 +0530

    v2

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 employees = ["Rajesh", "Mahesh", "Mukesh", "Gudiya"]
 salary = [200000, 10000, 50000, 150000]
 
-for e in employees:
-    print(f"{e}")
+count = len(employees)
 
-for s in salary:
-    print(f"{s}")
+for c in range(count):
+    print(f"{employees[c]}, {salary[c]}")
