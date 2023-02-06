1f85214808fb10d12ae6743ace41f5b05224ab84
rajeshmauryasde <rajeshmauryasde@gmail.com>
2023-02-06 13:19:48 +0530

    v5

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 class Employee:
-    def __init__(self, name, salary):
+    def __init__(self, name, salary, job_title):
         self.name = name
         self.salary = salary
+        self.job_title = job_title
 
 
 employees = [
-    Employee("Rajesh", 200000),
-    Employee("Mahesh", 10000),
-    Employee("Mukesh", 50000),
-    Employee("Gudiya", 150000),
+    Employee("Rajesh", 200000, "SDE"),
+    Employee("Mahesh", 10000, "DEVOPS"),
+    Employee("Mukesh", 50000, "DEVOPS"),
+    Employee("Gudiya", 150000, "SDE"),
 ]
 
 for e in employees:
-    if e.name == "Rajesh" or e.name == "Gudiya":
-        print(f"{e.name}, {e.salary}, SDE")
-    else:
-        print(f"{e.name}, {e.salary}, DEVOPS")
+    print(f"{e.name}, {e.salary}, {e.job_title}")
